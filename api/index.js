dc985e9906b4ca6c31490b6f4d843c13d32b4fef
FacuRearte <Facundorearte13@gmail.com>
2021-09-08 21:33:41 -0300

    holasda

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 const { preloadData } = require('./preloadData')
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(async() => {
+conn.sync({ force: false }).then(async() => {
   await preloadData()
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
