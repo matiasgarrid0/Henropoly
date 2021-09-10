3f134047b1e9972a7ea007448ebaedf6c61a2476
moni <you@example.com>
2021-09-10 15:19:22 -0300

    display-players components

--- client/src/components/playerProps/playerProps.jsx
+++ client/src/components/playerProps/playerProps.jsx
@@ -1,55 +1,73 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { register } from "../../redux/actions";
+/* import { register } from "../../redux/actions"; */
 import './playerProps.css'
 
 function PlayerProps(idsala) {
   const dispatch = useDispatch();
   const players = useSelector((state) => state.reducerInfo.infoGame);
-  const base1 = players[0].resultNewGame.PlayerData.target1;
-  const base2 = players[0].resultNewGame.PlayerData.target2;
-  const base3 = players[0].resultNewGame.PlayerData.target3;
-  const base4 = players[0].resultNewGame.PlayerData.target4;
-  console.log(players);
-
+ /* let a;
+  if(!players[0] === undefined) {
+    return a = {
+  base1 : players[0].resultNewGame.PlayerData.target1,
+  base2 :players[0].resultNewGame.PlayerData.target2,
+  base3 : players[0].resultNewGame.PlayerData.target3,
+  base4 : players[0].resultNewGame.PlayerData.target4 
+}
+} */
   return (
     <div>
       {players[0] === undefined ? (
         <div>
-          {/* { <span>{players.userNewGame}  </span>*/}
+
           ......... LOADING
         </div>
       ) : (
-        <div>
-          {" "}
-          <div className="playerProps-box">
+        <div className= 'playerProps-box-big'>
+       <div className="playerProps-box">
           <label>Players 1: </label>
-          <span>{base1.username} </span> 
-          <span>{base1.henryCoins} </span>
-          <span>{base1.properties.length >0? base1.properties.map((e)=> e) :<div>no tenes propis</div> } </span> 
-          <span>{base1.cards.length > 0 ? base1.cards.map((e)=> e): <div>Aún no tenés cartas </div>} </span>
-          </div>
-          <div className="playerProps-box"> 
+          <span>{ players[0].resultNewGame.PlayerData.target1.username} </span> 
+          <label>HenryCoins: </label>
+          <span>{ players[0].resultNewGame.PlayerData.target1.henryCoins} </span>
+          <label> Tecnologias: </label>
+          <span>{ players[0].resultNewGame.PlayerData.target1.properties.length >0?  players[0].resultNewGame.PlayerData.target1.properties.map((e)=> e) :<div>no tenes propis</div> } </span> 
+          <label> Cartas: </label>
+          <span>{ players[0].resultNewGame.PlayerData.target1.cards.length > 0 ?  players[0].resultNewGame.PlayerData.target1.cards.map((e)=> e): <div>Aún no tenés cartas </div>} </span>
+       </div>
+
+        <div className="playerProps-box"> 
           <label>Players 2: </label>
-          <span>{base2.username} </span>
-          <span>{base2.henryCoins} </span>
-          <span>{base2.properties.length > 0 ? base2.properties.map((e)=> e) : <div>Aún no tenés propiedades</div>} </span>
-          <span>{base2.cards.length > 0 ? base2.cards.map((e)=> e): <div>Aún no tenés cartas</div>} </span>
-          </div>               
-          <div className="playerProps-box"> 
+          <span>{players[0].resultNewGame.PlayerData.target2.username} </span>
+          <label>HenryCoins: </label>
+          <span>{players[0].resultNewGame.PlayerData.target2.henryCoins} </span>
+          <label> Tecnologias: </label>
+          <span>{players[0].resultNewGame.PlayerData.target2.properties.length > 0 ? players[0].resultNewGame.PlayerData.target2.properties.map((e)=> e) : <div>Aún no tenés propiedades</div>} </span>
+          <label> Cartas: </label>
+          <span>{players[0].resultNewGame.PlayerData.target2.cards.length > 0 ? players[0].resultNewGame.PlayerData.target2.cards.map((e)=> e): <div>Aún no tenés cartas</div>} </span>
+        </div>  
+
+         <div className="playerProps-box"> 
           <label>Players 3: </label>        
-          <span>{base3.username} </span>       
-          <span>{base3.henryCoins} </span>
-          <span>{base3.properties.length > 0 ? base3.properties.map((e)=> <div>e</div>) : <div>Aún no tenés propiedades</div>} </span>
-          <span>{base3.cards.length > 0 ? base3.cards.map((e)=> e): <div>Aún no tenés cartas</div>} </span>
-          </div>
-          <div className="playerProps-box">
+          <span>{ players[0].resultNewGame.PlayerData.target3.username} </span>
+          <label>HenryCoins: </label>       
+          <span>{ players[0].resultNewGame.PlayerData.target3.henryCoins} </span>
+          <label> Tecnologias: </label>
+          <span>{ players[0].resultNewGame.PlayerData.target3.properties.length > 0 ? players[0].resultNewGame.PlayerData.target3.properties.map((e)=> <div>e</div>) : <div>Aún no tenés propiedades</div>} </span>
+          <label> Cartas: </label>
+          <span>{ players[0].resultNewGame.PlayerData.target3.cards.length > 0 ?  players[0].resultNewGame.PlayerData.target3.cards.map((e)=> e): <div>Aún no tenés cartas</div>} </span>
+          <label> Color: (por ahoritas) </label>
+       </div>
+
+        <div className="playerProps-box">
           <label>Player 4: </label>
-          <span>{base4.username} </span>
-          <span>{base4.henryCoins} </span>
-          <span>{base4.properties.length >0? base4.properties.map((e)=> e) :<div>Aún no tenés propiedades</div> } </span> 
-          <span>{base4.cards.length > 0 ? base4.cards.map((e)=> e): <div>Aún no tenés cartas </div>} </span>
-          </div>
+          <span>{players[0].resultNewGame.PlayerData.target4 .username} </span>
+          <label>HenryCoins: </label>
+          <span>{players[0].resultNewGame.PlayerData.target4 .henryCoins} </span>
+          <label> Tecnologias: </label>
+          <span>{players[0].resultNewGame.PlayerData.target4 .properties.length >0? players[0].resultNewGame.PlayerData.target4 .properties.map((e)=> e) :<div>Aún no tenés propiedades</div> } </span> 
+          <label> Cartas: </label>
+          <span>{players[0].resultNewGame.PlayerData.target4 .length > 0 ? players[0].resultNewGame.PlayerData.target4 .cards.map((e)=> e): <div>Aún no tenés cartas </div>} </span>
+        </div>
         </div>
       )}
     </div>
