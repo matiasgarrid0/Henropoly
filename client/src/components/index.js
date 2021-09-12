3defb5a801f4100652b1168305bbdb83a88da93c
FlorenciaHQ <florenciahidalgoq@hotmail.com>
2021-09-12 20:56:53 -0300

    fix merge

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -9,7 +9,6 @@ import DisplayGame from './displayGame/DisplayGame.jsx'
 import GameDashBoard from './gameDashBoard/GameDashBoard.jsx'
 import ViewBoard from './ViewBoard/ViewBoard.jsx';
 import Background3D from './background3D/Background3D.jsx'
-<<<<<<< HEAD
 import Dices from './dice/Dice.jsx';
 import PlayerProps from './playerProps/playerProps.jsx';
 import Action from './playerProps/switchBoxBoard'
@@ -19,9 +18,7 @@ import RailwayCard from './railwayCard/RailwayCard'
 import ServiceCard  from './serviceCard/ServiceCard'
 import TaxCard from './taxCard/TaxCard'
 import CreateGame from './createGame/CreateGame'
-=======
 import Chat from './chat/Chat.jsx'
->>>>>>> main
 
 export {
     Loggin,
