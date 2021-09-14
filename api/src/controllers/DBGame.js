59e98818599edfe4381a746f519eaa57237a2e17
Senjo903 <joseleonardoagreda@gmail.com>
2021-09-14 12:23:13 -0300

    fix

--- api/src/controllers/DBGame.js
+++ api/src/controllers/DBGame.js
@@ -6,7 +6,6 @@ const redisConfig = {
 };
 const client = asyncRedis.createClient(redisConfig);
 var timers = {};
-
 const randomArray = (arr) => {
   const newArr = arr.slice();
   for (let i = newArr.length - 1; i > 0; i--) {
@@ -17,127 +16,137 @@ const randomArray = (arr) => {
 };
 const searchStatus = async (username) => {
   try {
-  const ResponsePlayersInHold = await client.get(`playersInHold${username}`)
-  if (ResponsePlayersInHold!== null) {
-    const responseWaitingRoom = await client.get(`waitingRoom${ResponsePlayersInHold}`)
-    return { status: "inHold", room: JSON.parse(responseWaitingRoom) }
-  }
-  const ResponsePlayersInGame = await client.get(`playersInGame${username}`)
-  if (ResponsePlayersInGame !== null) {
-    const responseGameRoom = await client.get(`gameRoom${ResponsePlayersInGame}`)
-    return {
-      status: "inGame",
-      data: JSON.parse(responseGameRoom)
+    const ResponsePlayersInHold = await client.get(`playersInHold${username}`)
+    if (ResponsePlayersInHold !== null) {
+      const responseWaitingRoom = await client.get(
+        `waitingRoom${ResponsePlayersInHold}`
+      );
+      return { status: "inHold", room: JSON.parse(responseWaitingRoom) };
     }
-  }
-  return { status: "free" };
+    const ResponsePlayersInGame = await client.get(`playersInGame${username}`);
+    if (ResponsePlayersInGame !== null) {
+      const responseGameRoom = await client.get(
+        `gameRoom${ResponsePlayersInGame}`
+      );
+      return {
+        status: "inGame",
+        data: JSON.parse(responseGameRoom),
+      };
+    }
+    return { status: "free" };
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 const createRoom = async (username, io) => {
   try {
-    const ResponsePlayersInHold = await client.get(`playersInHold${username}`)
-    const responseWaitingRoom = await client.get(`waitingRoom${ResponsePlayersInHold}`)
+    const ResponsePlayersInHold = await client.get(`playersInHold${username}`);
+    const responseWaitingRoom = await client.get(
+      `waitingRoom${ResponsePlayersInHold}`
+    );
     if (responseWaitingRoom === null && ResponsePlayersInHold === null) {
       const value = JSON.stringify({
         host: username,
         players: [],
       });
-      const playersInHold = await client.set(`playersInHold${username}`,username)
-      const waitingRoom = await client.set(`waitingRoom${username}`,value)
+      await client.set(`playersInHold${username}`, username);
+      await client.set(`waitingRoom${username}`, value);
       io.sockets.in(username).emit("roomStatus", {
         status: "inHold",
         room: { host: username, players: [] },
       });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 const deleteRoom = async (username, io) => {
   try {
-    const responseWaitingRoom = await client.get(`waitingRoom${username}`)
-    let playersFree = (JSON.parse(responseWaitingRoom).players)
-    playersFree.push(username)
-    await client.del(`waitingRoom${username}`)
+    const responseWaitingRoom = await client.get(`waitingRoom${username}`);
+    let playersFree = JSON.parse(responseWaitingRoom).players;
+    playersFree.push(username);
+    await client.del(`waitingRoom${username}`);
     playersFree.forEach(async (player) => {
-      await client.del(`playersInHold${username}`)
+      await client.del(`playersInHold${username}`);
       io.sockets.in(player).emit("roomStatus", {
         status: "free",
       });
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 const joinRoom = async (username, host, io) => {
   try {
-  const responseWaitingRoom = await client.get(`waitingRoom${host}`)
-  if(responseWaitingRoom !== null){
-    let roomJson = JSON.parse(responseWaitingRoom)
-    if(roomJson.host !== username &&
-      roomJson.players.length < 3 &&
-      !roomJson.players.includes(username)){
-      roomJson.players.push(username)
-      await client.set(`waitingRoom${host}`, JSON.stringify(roomJson))
-      await client.set(`playersInHold${username}`, host)
-      roomJson.players.forEach((player) => {
-        io.sockets.in(player).emit("roomStatus", {
-          status: "inHold",
-          room: roomJson
+    const responseWaitingRoom = await client.get(`waitingRoom${host}`);
+    if (responseWaitingRoom !== null) {
+      let roomJson = JSON.parse(responseWaitingRoom);
+      if (
+        roomJson.host !== username &&
+        roomJson.players.length < 3 &&
+        !roomJson.players.includes(username)
+      ) {
+        roomJson.players.push(username);
+        await client.set(`waitingRoom${host}`, JSON.stringify(roomJson));
+        await client.set(`playersInHold${username}`, host);
+        roomJson.players.forEach((player) => {
+          io.sockets.in(player).emit("roomStatus", {
+            status: "inHold",
+            room: roomJson,
+          });
         });
-      });
-      io.sockets.in(host).emit("roomStatus", {
+        io.sockets.in(host).emit("roomStatus", {
           status: "inHold",
-          room: roomJson
+          room: roomJson,
         });
+      }
     }
-  }
-    } catch (error) {
-    console.log(error)
+  } catch (error) {
+    console.log(error);
   }
 };
 const leaveRoom = async (username, io) => {
   try {
-  const ResponsePlayersInHold = await client.get(`playersInHold${username}`)
-  const responseWaitingRoom = await client.get(`waitingRoom${ResponsePlayersInHold}`)
-  let roomJson = (JSON.parse(responseWaitingRoom))
-  roomJson.players = roomJson.players.filter(
-    (player) => player !== username
-  )
-  await client.del(`playersInHold${username}`)
-  await client.set(`waitingRoom${ResponsePlayersInHold}`, JSON.stringify(roomJson))
-  roomJson.players.forEach((player) => {
-    io.sockets.in(player).emit("roomStatus", {
-      status: "inHold",
-      room: roomJson
+    const ResponsePlayersInHold = await client.get(`playersInHold${username}`);
+    const responseWaitingRoom = await client.get(
+      `waitingRoom${ResponsePlayersInHold}`
+    );
+    let roomJson = JSON.parse(responseWaitingRoom);
+    roomJson.players = roomJson.players.filter((player) => player !== username);
+    await client.del(`playersInHold${username}`);
+    await client.set(
+      `waitingRoom${ResponsePlayersInHold}`,
+      JSON.stringify(roomJson)
+    );
+    roomJson.players.forEach((player) => {
+      io.sockets.in(player).emit("roomStatus", {
+        status: "inHold",
+        room: roomJson,
+      });
     });
-  });
-  io.sockets.in(roomJson.host).emit("roomStatus", {
-    status: "inHold",
-    room: roomJson
-  });
-  io.sockets.in(username).emit("roomStatus", {
-    status: "free",
-  });
+    io.sockets.in(roomJson.host).emit("roomStatus", {
+      status: "inHold",
+      room: roomJson,
+    });
+    io.sockets.in(username).emit("roomStatus", {
+      status: "free",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
-
 const timer = (username, io) => {
   timers[username] = setInterval(
-    async() => {
+    async () => {
       console.log("cambio turno");
-      const responseGameRoom = await client.get(`gameRoom${username}`)
-      var roomGame = JSON.parse(responseGameRoom)
+      const responseGameRoom = await client.get(`gameRoom${username}`);
+      var roomGame = JSON.parse(responseGameRoom);
       roomGame.actualTurn = roomGame.order[1];
       let arrayOrder = roomGame.order;
       let playerFinal = arrayOrder.shift();
       arrayOrder.push(playerFinal);
       roomGame.order = arrayOrder;
-      await client.set(`gameRoom${username}`,JSON.stringify(roomGame))
+      await client.set(`gameRoom${username}`, JSON.stringify(roomGame));
       arrayOrder.forEach((player) => {
         io.sockets.in(player).emit("setGame", {
           status: "setTurns",
@@ -155,67 +164,67 @@ const clearTimer = (room) => {
 };
 const goGame = async (username, io) => {
   try {
-  const responseWaitingRoom = await client.get(`waitingRoom${username}`)
-    let playersFree = (JSON.parse(responseWaitingRoom).players)
-    playersFree.push(username)
-    await client.del(`waitingRoom${username}`)
+    const responseWaitingRoom = await client.get(`waitingRoom${username}`);
+    let playersFree = JSON.parse(responseWaitingRoom).players;
+    playersFree.push(username);
+    await client.del(`waitingRoom${username}`);
     playersFree.forEach(async (player) => {
-      await client.set(`playersInGame${player}`,username)
+      await client.set(`playersInGame${player}`, username);
     });
-  var orden = randomArray(playersFree);
-  var gameRoom = {
-    status: "inGame",
-    host: username,
-    order: orden,
-    actualTurn: orden[0],
-    dataPlayers: {
-      target3: { username: null, status: false, box: 0, x: 120, y: 40 },
-      target4: { username: null, status: false, box: 0, x: 40, y: 40 },
-    },
-  };
-  gameRoom.dataPlayers.target1 = {
-    username: gameRoom.order[0],
-    status: true,
-    box: 0,
-    x: 120,
-    y: 120,
-  };
-  gameRoom.dataPlayers.target2 = {
-    username: gameRoom.order[1],
-    status: true,
-    box: 0,
-    x: 40,
-    y: 120,
-  };
-  if (gameRoom.order[2]) {
-    gameRoom.dataPlayers.target2 = {
-      username: gameRoom.order[2],
+    var orden = randomArray(playersFree);
+    var gameRoom = {
+      status: "inGame",
+      host: username,
+      order: orden,
+      actualTurn: orden[0],
+      dataPlayers: {
+        target3: { username: null, status: false, box: 0, x: 120, y: 40 },
+        target4: { username: null, status: false, box: 0, x: 40, y: 40 },
+      },
+    };
+    gameRoom.dataPlayers.target1 = {
+      username: gameRoom.order[0],
       status: true,
       box: 0,
       x: 120,
-      y: 40,
+      y: 120,
     };
-  }
-  if (gameRoom.order[3]) {
     gameRoom.dataPlayers.target2 = {
-      username: gameRoom.order[3],
+      username: gameRoom.order[1],
       status: true,
       box: 0,
       x: 40,
-      y: 40,
+      y: 120,
     };
-  }
-  await client.set(`gameRoom${username}`, JSON.stringify(gameRoom))
-  playersFree.forEach(async (player) => {
-      await client.del(`playersInHold${player}`)
+    if (gameRoom.order[2]) {
+      gameRoom.dataPlayers.target2 = {
+        username: gameRoom.order[2],
+        status: true,
+        box: 0,
+        x: 120,
+        y: 40,
+      };
+    }
+    if (gameRoom.order[3]) {
+      gameRoom.dataPlayers.target2 = {
+        username: gameRoom.order[3],
+        status: true,
+        box: 0,
+        x: 40,
+        y: 40,
+      };
+    }
+    await client.set(`gameRoom${username}`, JSON.stringify(gameRoom));
+    playersFree.forEach(async (player) => {
+      await client.del(`playersInHold${player}`);
       io.sockets.in(player).emit("roomStatus", {
-          status: "inGame",
-          data: gameRoom
+        status: "inGame",
+        data: gameRoom,
       });
-  });
-  timer(username, io);
+    });
+    timer(username, io);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 module.exports = {
