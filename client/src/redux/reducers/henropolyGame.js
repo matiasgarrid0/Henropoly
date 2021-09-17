0d99d24fbf0e42824b45901a039f087faf3d4184
SebastianDelescabe <sebastiandelescabe@gmail.com>
2021-09-17 20:23:13 -0300

    logica v2

--- client/src/redux/reducers/henropolyGame.js
+++ client/src/redux/reducers/henropolyGame.js
@@ -1,4 +1,4 @@
-import { SET_GAME, SET_TARGET_VALUE, SET_TURNS, KICK_PLAYER, SET_GAME_STATUS, SET_GAME_ROLL, BUY_PROPERTY_ACTION, BUY_RAILWAY_ACTION, BUY_SERVICE_ACTION } from "../constants";
+import { SET_GAME, SET_TARGET_VALUE, SET_TURNS, KICK_PLAYER, SET_GAME_STATUS, SET_GAME_ROLL, BUY_PROPERTY_ACTION, BUY_RAILWAY_ACTION, BUY_SERVICE_ACTION, BUY_RAILWAY_ACTION } from "../constants";
 
 const initialState = {
   status: "free",
@@ -45,14 +45,21 @@ const henropolyGame = (state = initialState, action) => {
         status:payload   
        };
        case BUY_PROPERTY_ACTION:
-         let newTable= state.table 
-         console.log( "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", payload)
+         let newTable= state.table
          newTable[payload.box].owner = state.dataPlayers[payload.newProperty].username //propiedad dentro del payload
         return { 
           ...state,
          table: newTable,
          dataPlayers:{...state.dataPlayers, [payload.newProperty]:{...state.dataPlayers[payload.newProperty], henryCoin: payload.newbalase }}
         };
+        case BUY_RAILWAY_ACTION:
+           let newTable2= state.table
+           newTable2[payload.box].owner = state.dataPlayers[payload.newRailway].username
+          return { 
+           ...state,
+           table: newTable2,
+           dataPlayers:{...state.dataPlayers, [payload.newRailway]:{...state.dataPlayers[payload.newRailway], henryCoin: payload.newbalase }}
+          };
         // case BUY_SERVICE_ACTION:
         //   let newTableOne= state.table
         //   newTableOne[payload.box].owner = state.dataPlayers[payload.newService].username
